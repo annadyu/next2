e1449c86b9d9a590b683bf34b18dbf261c267895
dyuanna <annadyu666@gmail.com>
2025-12-08 01:44:36 +0900

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import React from "react";
 import MovieList from "./components/MovieList";
-// import App from "./components/Pagination";
 
 const MoviesPage = async () => {
   const res = await fetch(
-    `https://api.themoviedb.org/3/search/movie?query=return&api_key=${process.env.MOVIE_API_KEY}`,
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.MOVIE_API_KEY}`,
     { next: { revalidate: 60 } }
   );
   const data = await res.json();
